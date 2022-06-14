be83946e0abeb9b9f61613cce380545bc76398c0
MutiuMutiatD <105435527+MutiuMutiatD@users.noreply.github.com>
2022-06-14 10:09:40 +0100

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,34 @@
+
+
+from os import name
+
+
+class Pupil:
+    def __init__(self, name, age, track, score):
+        self.name = name
+        self.age = age
+        self.track = track
+        self.score = score
+    
+    def change_name(self, newname):
+        self.name = newname
+        print("My name is ", self.name)
+    
+    def change_age(self, newage):
+        self.age = newage
+        print("My age is ", self.age)
+
+    def add_track(self, newtrack):
+        self.track = newtrack
+        print("I am enrolled in ", self.track)
+
+    def get_score(self):
+        print("My score is ", self.score)
+
+Dami = Pupil(name="Dami", age=24, track="FullStack",score=36.92)
+
+# Expected methods
+Dami.change_name("Ife")
+Dami.change_age(29)
+Dami.add_track("FrontEnd")
+Dami.get_score()
